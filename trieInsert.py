bf8bb24a791655f1dbe2bcdbf9ebfbac04934a21
Unmystic <unmystic7@gmail.com>
2024-08-27 12:11:42 +0200

    Adjusted second dictionary insertion. Added creation of dict with definitions

--- trieInsert.py
+++ trieInsert.py
@@ -1,6 +1,8 @@
 from trie import Trie,TrieNode
 from dictread import create_dict
 import time
+import logging
+
 
 
 def insertDict(location, trie):
@@ -12,28 +14,31 @@ def insertDict(location, trie):
             # print(f"Word is : {word}")
 
 
+# Insertion words from smaller dictionary but with definitions 
 def insertDict2(location, trie):
     with open(location, "r", encoding="cp1251", newline="") as file:
+        # Creating dict with definitions for every word
         words = []
+        wordDict ={}
+
+        # Data from txt file is not clean, so we use filtering and formating of lines
         for line in file:
             line = line.rstrip('\r\n')
             if line:
-                # print(line)
                 try:
                     slovo,tolk = line.split(",", 1)
-                # tolk = tolk.split(". ", 1)
+                    _,tolk = tolk.split(". ", 1)
+                    slovo = slovo.title()
                     words.append(slovo)
+                    wordDict[slovo] = tolk
                 except ValueError:
                     continue
-        # for word in words[:10]:
-        #     slovo,tolk = word.split(",", 1)
-        #     mest, tolk = tolk.split(". ", 1)
-        #     print(slovo)
-        #     print(tolk)
-        print(words[:10])
+        # print(words[:10])
+        # print(len(words), len(wordDict))
+        # print(wordDict["Аббат"])
 
-        # for word in words:
-        #     trie.insert(word.rstrip())
+        for word in words:
+            trie.insert(word.rstrip())
 
 if __name__ == "__main__":
     start = time.time()
@@ -45,8 +50,8 @@ if __name__ == "__main__":
     print(f"Your programm executed in  {total_time} seconds")
 
     start = time.time()
-    print(trie.search("абажур"))
-    print(trie.search("размер"))
+    print(trie.search("Абажур"))
+    print(trie.search("Размер"))
     end = time.time()
     total_time = end - start
     print(f"Your programm executed in  {total_time} seconds")
